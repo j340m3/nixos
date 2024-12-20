548e20a2574ea37ad8bef005ab3baa16890ceb48
Jerome Bergmann <j340m3@users.noreply.github.com>
2024-12-20 12:59:23 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -22,7 +22,7 @@
   networking.domain = "";
   services.fail2ban.enable = true;
   services.openssh = {
-    ports = [22 80];
+    ports = [42069];
     enable = true;
     settings.PasswordAuthentication = false;
     settings.KbdInteractiveAuthentication = false;
