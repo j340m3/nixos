b714642630e4c6ead1f3aae34ed01774b5464508
Jerome Bergmann <j340m3@users.noreply.github.com>
2024-12-20 14:06:44 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -113,7 +113,12 @@
 services.nginx = {
   enable = true;
   virtualHosts.localhost = {
-    onlySSL = true;
+    #onlySSL = true;
+    listen = [{
+    addr = 127.0.0.1;
+    port = 443;
+    ssl = true;
+    }];
     locations."/" = {
       return = "200 '<html><body>It works</body></html>'";
       extraConfig = ''
