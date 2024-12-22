88349adf801f68aafdc953c02dc77da73f433564
Jerome Bergmann <j340m3@users.noreply.github.com>
2024-12-22 17:36:20 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -88,8 +88,8 @@
 
   nix.gc = {
     automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 30d";
+    dates = "hourly";
+    #options = "--delete-older-than 30d";
   };
 
   systemd.services.monitoring = {
