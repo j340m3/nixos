7ba8728ee8f2bbffceb18091120a248732c3c55a
Jerome Bergmann <j340m3@users.noreply.github.com>
2025-01-15 14:30:07 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,10 +1,12 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    inputs.sops-nix.url = "github:Mic92/sops-nix";
-    # optional, not necessary for the module
-    inputs.sops-nix.inputs.nixpkgs.follows = "nixpkgs";
     #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+
+    sops-nix.url = "github:Mic92/sops-nix";
+    # optional, not necessary for the module
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
+    
   };
   outputs = inputs@{ self, nixpkgs, sops-nix, ... }: {
     nixosConfigurations.nixos-gb = nixpkgs.lib.nixosSystem {
