cd6d9a0f093f4f015a20aeda758fc83e01899b6a
Jerome Bergmann <j340m3@users.noreply.github.com>
2025-01-15 13:48:31 +0100

    Update hardening.nix

--- hardening.nix
+++ hardening.nix
@@ -131,7 +131,7 @@ with lib;
     "-a exit,always -F arch=b64 -S execve"
   ];
   environment.etc."audit/auditd.conf".text = ''
-    num_logs = 99
+    num_logs = 5
     max_log_file = 50
     max_log_file_action = ROTATE
     space_left = 500
