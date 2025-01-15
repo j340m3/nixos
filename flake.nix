03e670eac34e7a180cc42da50ed2cc45308d4243
Jerome Bergmann <j340m3@users.noreply.github.com>
2025-01-15 09:47:08 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
-  outputs = { nixpkgs, ... }: {
+  outputs = inputs@{ self, nixpkgs, ... }: {
     nixosConfigurations.nixos-gb = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [ ./configuration.nix ];
