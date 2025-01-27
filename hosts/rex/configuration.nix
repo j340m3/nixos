001e664ea74bb0a5b0548d74f541e786f888d07c
j340m3 <j340m3@github.com>
2025-01-27 13:58:04 +0100

    i have to learn how to declare options xD

--- hosts/rex/configuration.nix
+++ hosts/rex/configuration.nix
@@ -110,7 +110,7 @@ let
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  config.allowReboot = false;
+  allowReboot = false;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.jeromeb = {
