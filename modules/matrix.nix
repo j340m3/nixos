cc6842ece5ef3a5c393bcf93bd587efc7419ae44
Jerome Bergmann <j340m3@users.noreply.github.com>
2025-03-07 10:03:22 +0100

    Create matrix.nix

---
+++ modules/matrix.nix
@@ -0,0 +1,86 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+# Adapted from:
+# https://gitlab.com/famedly/conduit/-/blob/3bfdae795d4d9ec9aeaac7465e7535ac88e47756/nix/README.md
+let
+  server_name = "kauderwels.ch";
+  matrix_hostname = "matrix.${server_name}";
+in {
+  services.matrix-conduit = {
+    enable = true;
+    package = pkgs.conduwuit_git;
+    settings.global = {
+      allow_registration = false;
+      database_backend = "rocksdb";
+      inherit server_name;
+      new_user_displayname_suffix = "";
+    };
+  };
+
+  # Fix for new binary name in 0.5.0
+  systemd.services.conduit.serviceConfig.ExecStart = lib.mkForce "${pkgs.conduwuit_git}/bin/conduwuit";
+
+  services.nginx = {
+    virtualHosts = {
+      "${matrix_hostname}" = {
+        extraConfig = ''
+          merge_slashes off;
+        '';
+        forceSSL = true;
+        http3 = true;
+        http3_hq = true;
+        kTLS = true;
+        listen = [
+          {
+            addr = "0.0.0.0";
+            port = 443;
+            ssl = true;
+          }
+          {
+            addr = "[::]";
+            port = 443;
+            ssl = true;
+          }
+          {
+            addr = "0.0.0.0";
+            port = 8448;
+            ssl = true;
+          }
+          {
+            addr = "[::]";
+            port = 8448;
+            ssl = true;
+          }
+        ];
+        locations."/_matrix/" = {
+          proxyPass = "http://backend_conduit$request_uri";
+          proxyWebsockets = true;
+          extraConfig = ''
+            proxy_set_header Host $host;
+            proxy_buffering off;
+          '';
+        };
+        quic = true;
+        useACMEHost = "kauderwels.ch";
+      };
+    };
+
+    upstreams = {
+      "backend_conduit" = {
+        servers = {
+          "[::1]:${toString config.services.matrix-conduit.settings.global.port}" = {};
+        };
+      };
+    };
+  };
+
+  environment.systemPackages = with pkgs; [signal-cli];
+
+  networking.firewall = {
+    allowedTCPPorts = [80 443 8448];
+    allowedUDPPorts = [80 443 8448];
+  };
