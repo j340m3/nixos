29ef485a5a6f2beea530bc85135a0252372cd76b
Jerome Bergmann <j340m3@users.noreply.github.com>
2025-03-21 11:49:28 +0100

    Update configuration.nix

--- hosts/pricklepants/configuration.nix
+++ hosts/pricklepants/configuration.nix
@@ -11,6 +11,7 @@
   swapDevices = [ { device = "/swapfile"; size = 1024; } ];
   boot.tmp.cleanOnBoot = true;
   zramSwap.enable = true;
+  bla
   
   environment.systemPackages = with pkgs; [
     #bc
