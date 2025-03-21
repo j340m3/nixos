c62302ac791fea0ac92fe6d057adf93196cdfde8
Jerome Bergmann <j340m3@users.noreply.github.com>
2025-03-21 11:44:56 +0100

    Update update.nix

--- hosts/common/update.nix
+++ hosts/common/update.nix
@@ -52,31 +52,25 @@
     };
 
     # Create an email notification service for failed jobs
-    systemd.services."notify-email@" =
-      let address = "system@kauderwels.ch";
-      in {
+    systemd.services."notify-telegram@" =
+    {
         enable = true;
         environment.SERVICE_ID = "%i";
         script = ''
           TEMPFILE=$(mktemp)
-          echo "From: ${address}" > $TEMPFILE
-          echo "To: ${address}" >> $TEMPFILE
-          echo "Subject: Failure in $SERVICE_ID" >> $TEMPFILE
           echo -e "\nGot an error with $SERVICE_ID\n\n" >> $TEMPFILE
           set +e
           systemctl status $SERVICE_ID >> $TEMPFILE
           set -e
-          ${pkgs.msmtp}/bin/msmtp \
-              --file=${config.homePath}/.config/msmtp/config \
-              --account=system \
-              ${address} < $TEMPFILE
+          source /etc/telegram.secrets
+          ${pkgs.curl}/bin/curl -s -X POST https://api.telegram.org/bot$BOT_TOKEN/sendMessage -d chat_id=$GROUP_ID -d text="$(cat $TEMPFILE)" > /dev/null
         '';
       };
 
     # Send an email whenever auto upgrade fails
     systemd.services.nixos-upgrade.onFailure =
-      lib.mkIf config.systemd.services."notify-email@".enable
-      [ "notify-email@%i.service" ];
+      lib.mkIf config.systemd.services."notify-telegram@".enable
+      [ "notify-telegram@%i.service" ];
 
   };
 }
