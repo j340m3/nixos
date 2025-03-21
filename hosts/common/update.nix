bd400ee5af37c347fc96e2e963b5a994b16736f9
Jerome Bergmann <j340m3@users.noreply.github.com>
2025-03-21 11:27:50 +0100

    Update update.nix

--- hosts/common/update.nix
+++ hosts/common/update.nix
@@ -50,5 +50,33 @@
       MemoryHigh = "800M";
       MemoryMax = "1G";
     };
+
+    # Create an email notification service for failed jobs
+    systemd.services."notify-email@" =
+      let address = "system@kauderwels.ch";
+      in {
+        enable = true;
+        environment.SERVICE_ID = "%i";
+        script = ''
+          TEMPFILE=$(mktemp)
+          echo "From: ${address}" > $TEMPFILE
+          echo "To: ${address}" >> $TEMPFILE
+          echo "Subject: Failure in $SERVICE_ID" >> $TEMPFILE
+          echo -e "\nGot an error with $SERVICE_ID\n\n" >> $TEMPFILE
+          set +e
+          systemctl status $SERVICE_ID >> $TEMPFILE
+          set -e
+          ${pkgs.msmtp}/bin/msmtp \
+              --file=${config.homePath}/.config/msmtp/config \
+              --account=system \
+              ${address} < $TEMPFILE
+        '';
+      };
+
+    # Send an email whenever auto upgrade fails
+    systemd.services.nixos-upgrade.onFailure =
+      lib.mkIf config.systemd.services."notify-email@".enable
+      [ "notify-email@%i.service" ];
+
   };
 }
