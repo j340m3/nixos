495f3aa1df01d57b8c63c0f108109744eb1dc2c5
Jerome Bergmann <j340m3@users.noreply.github.com>
2025-03-24 20:47:02 +0100

    Aktualisieren von configuration.nix

--- hosts/pricklepants/configuration.nix
+++ hosts/pricklepants/configuration.nix
@@ -119,13 +119,13 @@
   }; */
 
   security.sudo.wheelNeedsPassword = true;
-
+  /*
   programs.mosh.enable = true;
   networking.firewall.allowedTCPPorts = [ 443 ];
   networking.firewall.allowedUDPPortRanges = [
     { from = 60000; to = 61000; }
 
-  ];
+  ];*/
 services.nginx = {
   enable = true;
   virtualHosts.localhost = {
