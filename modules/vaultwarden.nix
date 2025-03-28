a2f0316b9284784c484c67c9b8a7c4206528a4b0
j340m3 <j340m3@github.com>
2025-03-28 13:34:23 +0100

    the rocket port changed

--- modules/vaultwarden.nix
+++ modules/vaultwarden.nix
@@ -19,12 +19,12 @@ with lib;
     virtualHosts."vaultwarden.kauderwels.ch" = {
       forceSSL = true;
       locations."/" = {
-        proxyPass = "http://127.0.0.1:8000";
+        proxyPass = "http://127.0.0.1:${toString config.services.vaultwarden.config.ROCKET_PORT}";
       };
       sslCertificate = "/etc/ssl/certs/kauderwels.ch_ssl_certificate.cer";
       sslCertificateKey = "/etc/ssl/certs/_.kauderwels.ch_private_key.key";
     };
   };
 
-  networking.firewall.allowedTCPPorts = [ 8000 ];
+  #networking.firewall.allowedTCPPorts = [ config.services.vaultwarden.config.ROCKET_PORT ];
 }
