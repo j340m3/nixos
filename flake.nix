d5a200c16dd71a88c1754a46483ab631a458cae5
j340m3 <j340m3@github.com>
2025-04-02 09:13:02 +0200

    stable nix for rex

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
 
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
@@ -51,7 +51,7 @@
         # sops-nix.nixosModules.sops
       ];
     };
-    nixosConfigurations.rex = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.rex = nixpkgs-stable.lib.nixosSystem {
       specialArgs = {inherit inputs outputs;};
       system = "x86_64-linux";
       modules = [ 
