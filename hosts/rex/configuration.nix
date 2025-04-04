d88f575a9c7b707585cbefdfeba28f34014cc21a
j340m3 <j340m3@github.com>
2025-04-04 09:53:24 +0200

    back to new syntax

--- hosts/rex/configuration.nix
+++ hosts/rex/configuration.nix
@@ -98,8 +98,8 @@ let
   #services.printing.enable = true;
 
   # Enable sound with pipewire.
-  #services.pulseaudio.enable = false;
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
+  #hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
