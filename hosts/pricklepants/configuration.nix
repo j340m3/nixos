ce263e639db86616ea29f1f52c80a187b094e7ce
j340m3 <j340m3@github.com>
2025-04-07 17:07:26 +0200

    better path usage

--- hosts/pricklepants/configuration.nix
+++ hosts/pricklepants/configuration.nix
@@ -68,7 +68,7 @@
   sops = {
     defaultSopsFile = ../../secrets/example.yaml;
     age = {
-      keyFile = /var/lib/sops-nix/key.txt;
+      keyFile = "/var/lib/sops-nix/key.txt";
     };
     secrets = {
       "telegram/bot_token" = {};
