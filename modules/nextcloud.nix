c382812a7f3c848d554fbfa502eec8a711d6e03c
Jerome Bergmann <j340m3@users.noreply.github.com>
2025-04-10 19:41:50 +0200

    some minor twerks

--- modules/nextcloud.nix
+++ modules/nextcloud.nix
@@ -4,6 +4,15 @@
     nginx.virtualHosts = {
       "nextcloud.kauderwels.ch" = {
         forceSSL = true;
+        sslCertificate = "/etc/ssl/certs/kauderwels.ch_ssl_certificate_chain.cer";
+        sslCertificateKey = "/etc/ssl/certs/_.kauderwels.ch_private_key.key";
+        listen = [
+          {
+            addr = "0.0.0.0";
+            port = 443;
+            ssl = true;
+          }
+        ];
       };
     };
     # 
