7d011e08ea8a0f8213c8152cc9411b3722bba28a
Jerome Bergmann <j340m3@users.noreply.github.com>
2025-04-12 12:30:16 +0200

    i need more swap for updates...

--- hosts/bootstrap/configuration.nix
+++ hosts/bootstrap/configuration.nix
@@ -27,6 +27,20 @@
     devices = ["/dev/vda"];
   };
 
+  services.swapspace.enable = true;
+  services.swapspace.settings = {
+    swappath="/nix/swapfile";
+    lower_freelimit=50;
+    upper_freelimit=70;
+    freetarget=60;
+    buffer_elasticity=100;
+    cache_elasticity=100;
+  };
+  boot.kernel.sysctl = { "vm.swappiness" = 5;};
+
+  boot.tmp.cleanOnBoot = true;
+  zramSwap.enable = true;
+
   # Timezone, change based on your location
   time.timeZone = "Europe/Berlin";
 
