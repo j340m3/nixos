846e4fad8dc17ce666c9907095189cdc4def3f03
Jerome Bergmann <j340m3@users.noreply.github.com>
2025-04-13 08:37:17 +0200

    try again...

--- hosts/buzz/configuration.nix
+++ hosts/buzz/configuration.nix
@@ -8,7 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      #../bootstrap/configuration.nix
+      ../bootstrap/configuration.nix
       (modulesPath + "/profiles/minimal.nix")
       (modulesPath + "/profiles/headless.nix")
       ../../modules/hardening.nix
