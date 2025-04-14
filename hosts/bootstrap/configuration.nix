77debd516e6530eb095ac3ee5c88e85b5285d0a4
Jerome Bergmann <j340m3@users.noreply.github.com>
2025-04-14 20:57:36 +0200

    fixing holes...

--- hosts/bootstrap/configuration.nix
+++ hosts/bootstrap/configuration.nix
@@ -85,7 +85,7 @@
   # Enable SSH server and listen on port 2222
   services.openssh = {
     enable = true;
-    ports = [2222];
+    ports = lib.mkDefault [2222];
     settings = {
       #PasswordAuthentication = false;
       #PermitRootLogin = lib.mkForce "prohibit-password";
