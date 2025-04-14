0060a4da135e35861173a8e50f5381f2a0e6d0cd
Jerome Bergmann <j340m3@users.noreply.github.com>
2025-04-14 20:31:58 +0200

    and port 514...

--- modules/logging.nix
+++ modules/logging.nix
@@ -20,7 +20,7 @@
 			*.*;auth,authpriv.*               -/var/log/syslog
 		'';
     extraConfig = ''
-      *.*  action(type="omfwd" target="10.0.0.3" port="10514" protocol="udp"
+      *.*  action(type="omfwd" target="10.0.0.3" port="514" protocol="udp"
             action.resumeRetryCount="100"
             queue.type="linkedList" queue.size="10000")
     '';
