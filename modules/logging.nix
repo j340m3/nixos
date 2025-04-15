d7b549055030768965923a0328a4739211d8fc85
Jerome Bergmann <j340m3@users.noreply.github.com>
2025-04-15 18:55:05 +0200

    revert previous stuff...

--- modules/logging.nix
+++ modules/logging.nix
@@ -4,7 +4,7 @@
   pkgs,
   ...
 } : {
-	
+
 	environment.systemPackages = with pkgs; [
 		rsyslog
 	];
@@ -14,14 +14,14 @@
 	'';
 	services.rsyslogd = {
 		enable = true;
-		defaultConfig = ''
+		/* defaultConfig = ''
 		# Enable remote logging over UDP
 		$ModLoad imudp
 		$UDPServerRun 514
 		# Forward all logs to a remote server
 		*.* @10.0.0.3:514
-		'';
-		/* defaultConfig = ''
+		''; */
+		defaultConfig = ''
 
 			auth,authpriv.*                    /var/log/auth.log
 			local7.*                          -/var/log/boot.log
@@ -30,12 +30,12 @@
 			kern.*                             /var/log/kern.log
 			user.*                            -/var/log/user.log
 			*.*;auth,authpriv.*               -/var/log/syslog
-		''; */
-	/* extraConfig = ''
+		'';
+	extraConfig = ''
 		*.*  action(type="omfwd" target="10.0.0.3" port="514" protocol="udp"
 			action.resumeRetryCount="100"
 			queue.type="linkedList" queue.size="10000")
 			*.* @10.0.0.3:514
-	''; */
+	'';
 	};
 }
