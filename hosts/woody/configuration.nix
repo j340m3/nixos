29ac74fa473be9596883b3592c9460e3c62bfcbd
Jerome Bergmann <j340m3@users.noreply.github.com>
2025-04-16 17:25:25 +0200

    more verbose firewall

--- hosts/woody/configuration.nix
+++ hosts/woody/configuration.nix
@@ -277,6 +277,7 @@ let
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
+  networking.firewall.logRefusedConnections = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
