1253ce0bccced9ce0ddfc7ee5dd89005edf340f9
Jerome Bergmann <j340m3@users.noreply.github.com>
2025-04-16 21:09:42 +0200

    maybe more explicit naming for the mesh

--- modules/zabbix.nix
+++ modules/zabbix.nix
@@ -5,7 +5,7 @@
   ...
 }:
 {
-  networking.firewall.interfaces.mesh.allowedTCPPorts = [ 10050 ];
+  networking.firewall.interfaces."nebula.mesh".allowedTCPPorts = [ 10050 ];
   services.zabbixAgent = {
       enable = true;
       server = "10.0.0.0/24";
