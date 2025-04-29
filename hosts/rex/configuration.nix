62ef6c545885ae97b3813366a686c727bc42de01
j340m3 <j340m3@github.com>
2025-04-29 10:17:27 +0200

    2 de's together is not a good idea xD

--- hosts/rex/configuration.nix
+++ hosts/rex/configuration.nix
@@ -83,14 +83,14 @@ let
   };
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  #services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
   #services.xserver.displayManager.gdm.enable = true;
   #services.xserver.displayManager.gdm.wayland = true;
   #services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.desktopManager.mate.enable = true;
+  #services.xserver.displayManager.lightdm.enable = true;
+  #services.xserver.desktopManager.mate.enable = true;
   #services.xserver.desktopManager.mate.enableWaylandSession = true;
 
   #services.xserver.desktopManager.mate.extraPanelApplets = [];
