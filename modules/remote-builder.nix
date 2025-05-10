8764eccd4c94aba660d612e42db8aac60e7dc61f
Jerome Bergmann <j340m3@users.noreply.github.com>
2025-05-10 10:36:29 +0200

    starting with remote builds

---
+++ modules/remote-builder.nix
@@ -0,0 +1,13 @@
+{
+  users.users.remotebuild = {
+    isNormalUser = true;
+    createHome = false;
+    group = "remotebuild";
+    openssh.authorizedKeys.keys = [''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINl53KG7Z2l3NSGDSOX5YzmnMzIXbWfnls0J6Hmzcpbv root@pricklepants''];
+    #openssh.authorizedKeys.keyFiles = [ ./remotebuild.pub ];
+  };
+
+  users.groups.remotebuild = {};
+
+  nix.settings.trusted-users = [ "remotebuild" ];
+}
