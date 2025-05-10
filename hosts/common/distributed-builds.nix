4db554b0d8379b42e1f1f3dc7c2332523a079732
Jerome Bergmann <j340m3@users.noreply.github.com>
2025-05-10 11:57:05 +0200

    rectifying distributed builds

--- hosts/common/distributed-builds.nix
+++ hosts/common/distributed-builds.nix
@@ -1,15 +1,24 @@
 { pkgs, ... }:
 {
   nix.distributedBuilds = true;
-  nix.settings.builders-use-substitutes = true;
+  #nix.settings.builders-use-substitutes = true;
 
   nix.buildMachines = [
     {
-      hostName = "10.0.0.3";
-      sshUser = "remotebuild";
-      sshKey = "/root/.ssh/remotebuild";
+      hostName = "builder";
+      #sshUser = "remotebuild";
+      #sshKey = "/root/.ssh/remotebuild";
       system = pkgs.stdenv.hostPlatform.system;
-      supportedFeatures = [ "nixos-test" "big-parallel" "kvm" ];
+      speedFactor = 2;
+      supportedFeatures = [ "nixos-test" "big-parallel" "kvm" "benchmark" ];
     }
   ];
+  programs.ssh.extraConfig = ''
+  Host builder
+    HostName 10.0.0.3
+    Port 42069
+    User remotebuild
+    IdentitiesOnly yes
+    IdentityFile /root/.ssh/remotebuild
+  '';
 }
