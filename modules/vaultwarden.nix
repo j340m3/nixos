83dd2f6d2455d4a73d80236241ff80540bffa23e
j340m3 <j340m3@github.com>
2025-05-12 16:10:21 +0200

    vaultwarden had its backups disabled...

--- modules/vaultwarden.nix
+++ modules/vaultwarden.nix
@@ -8,13 +8,14 @@
 with lib;
 
 {
-  services.vaultwarden.enable = true;
-  services.vaultwarden.config = {
-    ROCKET_ADDRESS = "127.0.0.1";
-    ROCKET_PORT = 8222;
+  services.vaultwarden = {
+    enable = true;
+    config = {
+      ROCKET_ADDRESS = "127.0.0.1";
+      ROCKET_PORT = 8222;
+    };
     backupDir = "/var/backup/vaultwarden";
-  };
-
+  }; 
   services.nginx = {
     enable = true;
     
