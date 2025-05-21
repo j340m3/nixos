92dd6c38ecb975737fe47903ad30dff38d6bf80b
Jerome Bergmann <j340m3@users.noreply.github.com>
2025-05-21 15:57:27 +0200

    second bedrock server

--- modules/minecraft-bedrock.nix
+++ modules/minecraft-bedrock.nix
@@ -5,7 +5,7 @@
   virtualisation.podman.enable = true;
   virtualisation.oci-containers.backend = "podman";
   virtualisation.oci-containers.containers = {
-    minecraft = {
+    klassenserver = {
       environment = {
         ALLOW_CHEATS = "true";
         EULA = "TRUE";
@@ -18,6 +18,20 @@
       image = "itzg/minecraft-bedrock-server";
       ports = ["19132:19132/udp"];
       volumes = [ "/nix/persist/minecraft/klassenserver/:/data" ];
+    };
+    emeliebjorn = {
+      environment = {
+        ALLOW_CHEATS = "true";
+        EULA = "TRUE";
+        DIFFICULTY = "1";
+        SERVER_NAME = "Klassenserver";
+        TZ = "Europe/Berlin";
+        VERSION = "LATEST";
+        #ALLOW_LIST_USERS = "adyxax:2535470760215402,pseudo2:XXXXXXX,pseudo3:YYYYYYY";
+      };
+      image = "itzg/minecraft-bedrock-server";
+      ports = ["19132:19132/udp"];
+      volumes = [ "/nix/persist/minecraft/emeliebjorn/:/data" ];
     };
   };
   networking.firewall.allowedUDPPorts = [
