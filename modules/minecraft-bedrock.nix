4cba99a950cef6382ae935813438563cf7247c0f
Jerome Bergmann <j340m3@users.noreply.github.com>
2025-05-21 15:58:21 +0200

    needs a different port

--- modules/minecraft-bedrock.nix
+++ modules/minecraft-bedrock.nix
@@ -24,13 +24,13 @@
         ALLOW_CHEATS = "true";
         EULA = "TRUE";
         DIFFICULTY = "1";
-        SERVER_NAME = "Klassenserver";
+        SERVER_NAME = "Emelie Bjorn";
         TZ = "Europe/Berlin";
         VERSION = "LATEST";
         #ALLOW_LIST_USERS = "adyxax:2535470760215402,pseudo2:XXXXXXX,pseudo3:YYYYYYY";
       };
       image = "itzg/minecraft-bedrock-server";
-      ports = ["19132:19132/udp"];
+      ports = ["19132:19133/udp"];
       volumes = [ "/nix/persist/minecraft/emeliebjorn/:/data" ];
     };
   };
