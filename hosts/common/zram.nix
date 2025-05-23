80021fce3b5adfeb9a312e5e3029c02069da9f47
j340m3 <j340m3@github.com>
2025-05-23 15:05:58 +0200

    popos settings for zram

--- hosts/common/zram.nix
+++ hosts/common/zram.nix
@@ -4,4 +4,10 @@
     algorithm = lib.mkDefault "lz4";
     memoryPercent = 90;
   };
+  boot.kernel.sysctl = { 
+    "vm.swappiness" = 180;
+    "vm.watermark_boost_factor" = 0;
+    "vm.watermark_scale_factor" = 125;
+    "vm.page-cluster" = 0;
+  };
 }
