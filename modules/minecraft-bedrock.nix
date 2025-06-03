6857e21747634a7cf3f26ce2c00cd8cce18d1517
Jerome Bergmann <j340m3@users.noreply.github.com>
2025-06-03 18:29:12 +0200

    autoupgrade pods

--- modules/minecraft-bedrock.nix
+++ modules/minecraft-bedrock.nix
@@ -45,4 +45,25 @@
     19132 # Klassenserver
     19133 # emelie bjorn
   ];
+
+  systemd.timers.update-containers = {
+    timerConfig = {
+      Unit = "update-containers.service";
+      OnCalendar = "Mon 02:00";
+    };
+    wantedBy = [ "timers.target" ];
+  };
+  
+  systemd.services.update-containers = {
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = lib.getExe (pkgs.writeShellScriptBin "update-containers" ''
+        images=$(${pkgs.podman}/bin/podman ps -a --format="{{.Image}}" | sort -u)
+
+        for image in $images; do
+          ${pkgs.podman}/bin/podman pull "$image"
+        done
+      '');
+    };
+  };
 }
