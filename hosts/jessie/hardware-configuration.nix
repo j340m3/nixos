d292cea9667de5edaee6db92fb50fc529f3a90ad
j340m3 <j340m3@github.com>
2025-06-13 08:11:40 +0200

    asking a lot from jessie, increasing swap

--- hosts/jessie/hardware-configuration.nix
+++ hosts/jessie/hardware-configuration.nix
@@ -63,6 +63,8 @@
     interval = "weekly";
   };
 
+  swapDevices = [ { device = "/nix/persist/swapfile"; size = 2048; } ];
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
