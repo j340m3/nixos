95d87cbd5b4e1febd3714a784cc9366b6f24d5d1
j340m3 <j340m3@github.com>
2025-06-24 10:25:33 +0200

    automatic display resizing on rex

--- desktop-environments/mate.nix
+++ desktop-environments/mate.nix
@@ -2,4 +2,7 @@
   services.xserver.enable = true;
   services.xserver.displayManager.lightdm.enable = true;
   services.xserver.desktopManager.mate.enable = true;
+
+  # Automatic Display resizbe
+  services.spice-vdagentd.enable = true;
 }
