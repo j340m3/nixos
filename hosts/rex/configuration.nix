909fe0809e8daef2ce4a0fb1cdc8726feb67b0e4
j340m3 <j340m3@github.com>
2025-06-24 08:59:53 +0200

    activating pycharm again

--- hosts/rex/configuration.nix
+++ hosts/rex/configuration.nix
@@ -153,7 +153,7 @@ let
        telegram-desktop
        (makeAutostartItem { name = "telegram.desktop"; package = telegram-desktop; srcPrefix = "org.";})
        telegram-desktop
-       #jetbrains.pycharm-professional
+       jetbrains.pycharm-professional
        elmPackages.elm
        python3Full
        (python311.withPackages(ps: with ps; [ 
