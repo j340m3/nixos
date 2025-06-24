e09e341c5f2a07295d0813678ba06dac87801021
j340m3 <j340m3@github.com>
2025-06-24 09:30:04 +0200

    trying to squeeze more govenors out of woody

--- hosts/woody/hardware-configuration.nix
+++ hosts/woody/hardware-configuration.nix
@@ -12,6 +12,7 @@
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
+  boot.kernelParams = ["intel_pstate=passive"];
 
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/9bf7dc58-7c06-4f84-a028-84f59ae08bc8";
