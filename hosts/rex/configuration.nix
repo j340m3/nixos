e2b5e1fb6ac4acd1e994314b4845abfb17b198d0
j340m3 <j340m3@github.com>
2025-06-24 14:37:31 +0200

    more privacy

--- hosts/rex/configuration.nix
+++ hosts/rex/configuration.nix
@@ -261,6 +261,16 @@ let
         # This is annoying, because I can have a blank workspace, click Firefox from the launcher, and
         # then have Firefox open on some other workspace.
         "widget.disable-workspace-management" = true;
+
+        "cookiebanners.service.mode.privateBrowsing" = 2; # Block cookie banners in private browsing
+        "cookiebanners.service.mode" = 2; # Block cookie banners
+        "privacy.donottrackheader.enabled" = true;
+        "privacy.fingerprintingProtection" = true;
+        "privacy.resistFingerprinting" = true;
+        "privacy.trackingprotection.emailtracking.enabled" = true;
+        "privacy.trackingprotection.enabled" = true;
+        "privacy.trackingprotection.fingerprinting.enabled" = true;
+        "privacy.trackingprotection.socialtracking.enabled" = true;
       };
 
       ExtensionSettings = {
