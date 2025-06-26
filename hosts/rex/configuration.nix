400a73f20bda301fb3974c3dd828f5a7b8c8e252
j340m3 <j340m3@github.com>
2025-06-26 12:50:43 +0200

    and the dependency for ruff

--- hosts/rex/configuration.nix
+++ hosts/rex/configuration.nix
@@ -178,6 +178,7 @@ let
            jnoortheen.nix-ide
            bbenoist.nix
            elmtooling.elm-ls-vscode
+           ms-python.python
            charliermarsh.ruff
          ];
          vscode = vscodium;
