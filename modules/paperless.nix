1b7f8415e8ba4c9e24927401c5e2eca2fa993750
Jerome Bergmann <j340m3@users.noreply.github.com>
2025-07-05 23:18:51 +0200

    new version of tika usage

--- modules/paperless.nix
+++ modules/paperless.nix
@@ -9,47 +9,25 @@
   services.paperless = {
     enable = true;
     passwordFile = "/etc/paperless-admin-pass";
+    configureTika = true;
+    database.createLocally = true;
+    dataDir = "/apps/paperless";
     settings = {
-      PAPERLESS_TIKA_ENABLED = true;
-      PAPERLESS_TIKA_ENDPOINT = "http://127.0.0.1:${toString config.services.tika.port}";
-      PAPERLESS_TIKA_GOTENBERG_ENDPOINT = "http://127.0.0.1:${toString config.services.gotenberg.port}";
+      PAPERLESS_TASK_WORKERS = 1;
+      PAPERLESS_THREADS_PER_WORKER = 2;
       PAPERLESS_OCR_LANGUAGE = "deu+eng";
-      # PAPERLESS_APP_TITLE = "paperless.chungus.private";
-      PAPERLESS_CONSUMER_IGNORE_PATTERN = builtins.toJSON [
-        ".DS_STORE/*"
-        "desktop.ini"
-      ];
-      PAPERLESS_EMAIL_TASK_CRON = "0 */8 * * *"; # “At minute 0 past every 8th hour.”
+      # # PAPERLESS_APP_TITLE = "paperless.chungus.private";
+      # PAPERLESS_CONSUMER_IGNORE_PATTERN = builtins.toJSON [
+      #   ".DS_STORE/*"
+      #   "desktop.ini"
+      # ];
+      # PAPERLESS_EMAIL_TASK_CRON = "0 */8 * * *"; # “At minute 0 past every 8th hour.”
 
-      # https://github.com/paperless-ngx/paperless-ngx/discussions/4047#discussioncomment-7019544
-      # https://github.com/paperless-ngx/paperless-ngx/issues/7383
-      PAPERLESS_OCR_USER_ARGS = {
-        "invalidate_digital_signatures" = true;
-      };
-      #PAPERLESS_CONVERT_TMPDIR = "/var/lib/paperless/tmp";
-      #PAPERLESS_CONVERT_TMPDIR = "/var/tmp/paperless";
-      #PAPERLESS_SCRATCH_DIR = "/var/lib/paperless-scratch/tmp";
-    };
-  };
-
-  services.tika = {
-    enable = true;
-    enableOcr = true;
-    package = pkgs.tika;
-  };
-
-  services.gotenberg = {
-    enable = true;
-    timeout = "300s";
-    port = 3214;
-  };
-
-  systemd.services.gotenberg = {
-    environment.HOME = "/run/gotenberg";
-    serviceConfig = {
-      SystemCallFilter = lib.mkAfter [ "@chown" ]; # TODO remove when fixed (https://github.com/NixOS/nixpkgs/issues/349123)
-      WorkingDirectory = "/run/gotenberg";
-      RuntimeDirectory = "gotenberg";
+      # # https://github.com/paperless-ngx/paperless-ngx/discussions/4047#discussioncomment-7019544
+      # # https://github.com/paperless-ngx/paperless-ngx/issues/7383
+      # PAPERLESS_OCR_USER_ARGS = {
+      #   "invalidate_digital_signatures" = true;
+      # };  
     };
   };
 
