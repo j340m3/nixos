461fa93d55a8e0e0263089129ab8de7e17ecda43
Jerome Bergmann <j340m3@users.noreply.github.com>
2025-07-05 21:33:54 +0200

    paperless with tika and gotenberg

--- modules/paperless.nix
+++ modules/paperless.nix
@@ -1,8 +1,61 @@
-{ config, pkgs, lib, ...} : {
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+{
   environment.etc."paperless-admin-pass".text = "admin";
   services.paperless = {
     enable = true;
     passwordFile = "/etc/paperless-admin-pass";
-  };
-}
+    settings = {
+      PAPERLESS_TIKA_ENABLED = true;
+      PAPERLESS_TIKA_ENDPOINT = "http://127.0.0.1:${toString config.services.tika.port}";
+      PAPERLESS_TIKA_GOTENBERG_ENDPOINT = "http://127.0.0.1:${toString config.services.gotenberg.port}";
+      PAPERLESS_OCR_LANGUAGE = "deu+eng";
+      # PAPERLESS_APP_TITLE = "paperless.chungus.private";
+      PAPERLESS_CONSUMER_IGNORE_PATTERN = builtins.toJSON [
+        ".DS_STORE/*"
+        "desktop.ini"
+      ];
+      PAPERLESS_EMAIL_TASK_CRON = "0 */8 * * *"; # “At minute 0 past every 8th hour.”
 
+      # https://github.com/paperless-ngx/paperless-ngx/discussions/4047#discussioncomment-7019544
+      # https://github.com/paperless-ngx/paperless-ngx/issues/7383
+      PAPERLESS_OCR_USER_ARGS = {
+        "invalidate_digital_signatures" = true;
+      };
+    };
+  };
+
+  services.permown."/var/lib/paperless/consume" = {
+    owner = "paperless";
+    group = "paperless";
+    directory-mode = "755";
+    file-mode = "640";
+  };
+
+  services.tika = {
+    enable = true;
+    port = port;
+    enableOcr = true;
+    package = pkgs.tika;
+  };
+
+  services.gotenberg = {
+    enable = true;
+    timeout = "300s";
+    port = 3214;
+  };
+
+  systemd.services.gotenberg = {
+    environment.HOME = "/run/gotenberg";
+    serviceConfig = {
+      SystemCallFilter = lib.mkAfter [ "@chown" ]; # TODO remove when fixed (https://github.com/NixOS/nixpkgs/issues/349123)
+      WorkingDirectory = "/run/gotenberg";
+      RuntimeDirectory = "gotenberg";
+    };
+  };
+
+}
