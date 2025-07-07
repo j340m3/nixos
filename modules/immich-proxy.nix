c44802a2adf908475599bbdc1a0f502e33e1eaa8
j340m3 <j340m3@github.com>
2025-07-07 15:11:17 +0200

    maybe as proxypass?

--- modules/immich-proxy.nix
+++ modules/immich-proxy.nix
@@ -10,13 +10,16 @@
         forceSSL = true;
         sslCertificate = "/etc/ssl/certs/kauderwels.ch_ssl_certificate_chain.cer";
         sslCertificateKey = "/etc/ssl/certs/_.kauderwels.ch_private_key.key";
-        listen = [
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:${toString config.services.immich-public-proxy.port}";
+        };
+        /* listen = [
           {
             addr = "0.0.0.0";
             port = 3001;
             ssl = true;
           }
-        ];
+        ]; */
       };
   };
 }
