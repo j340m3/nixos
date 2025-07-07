1a887bdcec0e8b12b461a3bc862e02522e1cee9a
j340m3 <j340m3@github.com>
2025-07-07 14:57:34 +0200

    adding proxy

--- modules/immich-proxy.nix
+++ modules/immich-proxy.nix
@@ -1,7 +1,21 @@
 { self, config, lib, pkgs, ... }: {
-  simmich-public-proxy = {
+  services.immich-public-proxy = {
       enable = true;
       immichUrl = "http://10.0.0.3:2283";
       port = 3001;
     };
+    
+  services.nginx.virtualHosts = {
+      "immich.kauderwels.ch" = {
+        forceSSL = true;
+        sslCertificate = "/etc/ssl/certs/kauderwels.ch_ssl_certificate_chain.cer";
+        sslCertificateKey = "/etc/ssl/certs/_.kauderwels.ch_private_key.key";
+        listen = [
+          {
+            addr = "0.0.0.0";
+            port = 3001;
+            ssl = true;
+          }
+        ];
+      };
 }
