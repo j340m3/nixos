eeb79fe1a8c38478c9c94cdad5185be83cf5ad50
j340m3 <j340m3@github.com>
2025-08-18 14:58:25 +0200

    moving paperless to the nas

--- modules/paperless.nix
+++ modules/paperless.nix
@@ -11,7 +11,7 @@
     passwordFile = "/etc/paperless-admin-pass";
     configureTika = true;
     database.createLocally = true;
-    #dataDir = "/apps/paperless";
+    dataDir = "/mnt/nas/paperless";
     settings = {
       PAPERLESS_TASK_WORKERS = 1;
       PAPERLESS_THREADS_PER_WORKER = 2;
@@ -31,4 +31,38 @@
     };
   };
 
+
+  fileSystems."/mnt/nas/paperless" = {
+  device = "paperless:paperless";
+  fsType = "rclone";
+  options = [
+    "nodev"
+    #"_netdev"
+    #"nofail"
+    "noauto"
+    "allow_other"
+    "args2env"
+    "x-systemd.automount"
+    "cache_dir=/var/cache/rclone"
+    "dir-cache-time=24h"
+    "sftp-path-override=/volume1/borgbackup/rsyslog"
+    "vfs-cache-mode=full"
+    "vfs-cache-min-free-space=10G"
+    "vfs-fast-fingerprint"
+    "vfs-write-back=1m" # write changes after one hour
+    "vfs-cache-max-age=24h"                    # Retain cached files for up to 24 hours
+    "vfs-read-chunk-size=32M"                  # Start with 32MB chunks for faster initial reads
+    "vfs-read-chunk-size-limit=1G"             # Allow chunk size to grow up to 1GB for large files
+    "vfs-cache-poll-interval=30s" 
+    "tpslimit=8"
+    "tpslimit-burst=16"
+    "bwlimit=1K"
+    "transfers=4"
+    "config=/etc/rclone-mnt.conf"
+    "x-systemd.requires=network-online.target"
+    "x-systemd.after=network-online.target" # only after network came up
+    #"uid=${toString config.users.users.immich.uid}"
+    #"gid=${config.users.users.immich.group}"
+  ];
+  };
 }
