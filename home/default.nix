a24b84d4fda295fb9328600c44740c43568c6ec1
Jerome Bergmann <j340m3@users.noreply.github.com>
2025-09-24 06:25:23 +0000

    Update default.nix

--- home/default.nix
+++ home/default.nix
@@ -8,4 +8,8 @@
     home.homeDirectory = "/home/jeromeb";    
     home.stateVersion = "25.11"; # Comment out for error with "latest" version
     programs.home-manager.enable = true;
+    home.packages = with pkgs; [
+      btop
+      git
+    ];
 }
