15848c50584cf23e40e23460a00b03761703c137
Jerome Bergmann <j340m3@users.noreply.github.com>
2025-09-28 20:21:49 +0200

    Nope that doesnt work for me

--- hosts/buzz/default.nix
+++ hosts/buzz/default.nix
@@ -63,11 +63,6 @@
   # Enable the X11 windowing system.
   # services.xserver.enable = true;
   
-  programs.command-not-found = {
-    enable = true;
-  }
-  environment.variables.NIX_AUTO_RUN = "1";
-  
 
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
