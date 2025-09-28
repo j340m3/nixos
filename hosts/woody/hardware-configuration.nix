c1025af043e98ee3df0f87b09bcd1a6417530b06
Jerome Bergmann <j340m3@users.noreply.github.com>
2025-09-28 11:50:40 +0200

    woody writeback partition

--- hosts/woody/hardware-configuration.nix
+++ hosts/woody/hardware-configuration.nix
@@ -26,8 +26,10 @@
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/0eba583c-551f-48b0-b920-78bcd0c99521"; }
+    [ 
+      #{ device = "/dev/disk/by-uuid/0eba583c-551f-48b0-b920-78bcd0c99521"; }
     ];
+  writebackPartition = "/dev/disk/by-uuid/0eba583c-551f-48b0-b920-78bcd0c99521";
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
--- modules/common/zram.nix
+++ modules/common/zram.nix
@@ -1,16 +1,26 @@
 {config,lib,pkgs,...}:{
-  zramSwap = {
-    enable = true;
-    algorithm = lib.mkDefault "lz4";
-    memoryPercent = 100;
+  options = {
+    writebackPartition = lib.mkOption {
+      type = lib.types.string;
+      default = "";
+      description = "The writeback partition fro zramSwap.";
+    };
   };
-  boot.kernel.sysctl = { 
-    "vm.swappiness" = lib.mkDefault 180;
-    "vm.watermark_boost_factor" = 0;
-    "vm.watermark_scale_factor" = 125;
-    "vm.page-cluster" = 0;
-    "vm.dirty_background_ratio"=1;
-    "vm.dirty_ratio"=50;
-    "vm.vfs_cache_pressure"=500;
+  config = {
+    zramSwap = {
+      enable = true;
+      algorithm = lib.mkDefault "lz4";
+      memoryPercent = 100;
+      writebackPartition = lib.mkIf (config.writebackPartition != "") config.writebackPartition;
+    };
+    boot.kernel.sysctl = { 
+      "vm.swappiness" = lib.mkDefault 180;
+      "vm.watermark_boost_factor" = 0;
+      "vm.watermark_scale_factor" = 125;
+      "vm.page-cluster" = 0;
+      "vm.dirty_background_ratio"=1;
+      "vm.dirty_ratio"=50;
+      "vm.vfs_cache_pressure"=500;
+    };
   };
 }
