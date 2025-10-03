809d8bd26b11386e6c60d17155f48cc9b00c19eb
Jerome Bergmann <j340m3@users.noreply.github.com>
2025-10-03 12:35:14 +0200

    still missing the user....

--- hosts/lenny/jeromeb/default.nix
+++ hosts/lenny/jeromeb/default.nix
@@ -3,12 +3,13 @@
     ../../../desktop-environments/xfce.nix
     inputs.home-manager.nixosModules.home-manager
   ];
-
+  users.mutableUsers = false;
   system.nixos.tags = [ "jeromeb" ];
   users.users.jeromeb = {
     isNormalUser = true;
     description = "Jerome Bergmann";
     extraGroups = [ "networkmanager" "wheel" ];
+    hashedPassword = "$y$j9T$xI3Gvnwn4Q900uL0HQZHp/$fl5oFfnZWZWBz.6gPxvciND13komHHAXDqq6Yfpjn17";
     uid = 1002;
     packages = with pkgs; [
       firefox
