181d6f061793a72ca2ed2c6329e5c5848c475901
j340m3 <j340m3@github.com>
2025-10-16 13:58:27 +0200

    gtk whitesur install

--- home/xfce/default.nix
+++ home/xfce/default.nix
@@ -3,6 +3,33 @@
 {
   gtk = {
     enable = true;
+    theme = {
+      package = pkgs.whitesur-gtk-theme;
+      name = "WhiteSur-dark-solid";
+    };
+    # iconTheme = {
+    #   package = pkgs.whitesur-icon-theme.override {
+    #     alternativeIcons = true;
+    #     boldPanelIcons = true;
+    #   };
+    #   name = "WhiteSur";
+    # };
+    gtk3.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+    gtk4.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+  };
+
+  qt.kvantum.theme = {
+    package = pkgs.my.whitesur-kde;
+    dir = "WhiteSur-solid";
+    name = "WhiteSur-solid";
   };
   stylix.targets.xfce.enable = true;
 
