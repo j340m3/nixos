60c61b1d1301098ec6bf3b85a4848cf9d752cf2d
Jerome Bergmann <j340m3@users.noreply.github.com>
2025-10-23 20:41:49 +0200

    reactivating bare minimum

--- hosts/headless/pricklepants/default.nix
+++ hosts/headless/pricklepants/default.nix
@@ -9,12 +9,12 @@ in
     ../../../modules/hardening.nix
     #../../../modules/matrix2.nix
     #../../../modules/minetest.nix
-    #../../../modules/vaultwarden.nix
+    ../../../modules/vaultwarden.nix
     ../../../modules/logging.nix
     ../../../modules/zabbix.nix
     #../../../modules/nextcloud.nix
-    #../../../modules/dns-tunnel.nix
-    #../../../modules/immich-proxy.nix
+    ../../../modules/dns-tunnel.nix
+    ../../../modules/immich-proxy.nix
     ../../../modules/common
   ];
   #swapDevices = [ { device = "/swapfile"; size = 512; } ];
