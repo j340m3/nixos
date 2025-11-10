6e7ce81af3dd5a2dc28b5884312a5e28be475ff9
j340m3 <j340m3@github.com>
2025-11-10 13:43:44 +0100

    opening up the http interface

--- modules/technitium.nix
+++ modules/technitium.nix
@@ -1,9 +1,24 @@
-{...}:{
+{constants, config, lib, ...}:{
   services.technitium-dns-server = {
     enable = true;
     openFirewall = true;
   };
-  
+  networking.firewall.interfaces."nebula.mesh".allowedTCPPorts = [ 5380 53443];
+  services.nebula.networks.mesh.firewall.inbound = lib.mkIf 
+              (config.services.technitium-dns-server.enable && 
+              config.services.nebula.networks.mesh.enable) 
+      [
+        {
+          cidr = constants.nebula.cidr;
+          port = 5380;
+          proto = "any";
+        }
+        {
+          cidr = constants.nebula.cidr;
+          port = 53443;
+          proto = "any";
+        }
+      ];
   # networking.firewall.allowedUDPPorts = [
   #   53 # technitium-dns-server
   # ];
