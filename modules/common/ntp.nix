00def22ad9ff4a9381c404dd2f5fd9c5450621fb
j340m3 <j340m3@github.com>
2025-12-05 09:38:18 +0100

    still wrong syntax

--- modules/common/ntp.nix
+++ modules/common/ntp.nix
@@ -3,12 +3,12 @@
     enable = true;
     enableNTS = true;
     servers = [
-      "time.cloudflare.com iburst nts"
-      "ntppool1.time.nl iburst nts"
-      "nts.netnod.se iburst nts"
-      "ptbtime1.ptb.de iburst nts"
-      "time.dfm.dk iburst nts"
-      "time.cifelli.xyz iburst nts"
+      "time.cloudflare.com"
+      "ntppool1.time.nl"
+      "nts.netnod.se"
+      "ptbtime1.ptb.de"
+      "time.dfm.dk"
+      "time.cifelli.xyz"
     ];
   };
 }
