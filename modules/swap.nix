e064038fd0c1b2d535c54a03da2d551e561bfb2f
j340m3 <j340m3@github.com>
2025-12-05 11:27:51 +0100

    repairing swapspace for btrfs

--- modules/swap.nix
+++ modules/swap.nix
@@ -10,6 +10,9 @@
     buffer_elasticity=100;
     cache_elasticity=100;
   };
+  # Source: https://manpages.ubuntu.com/manpages/focal/man8/swapspace.8.html
+  services.swapspace.extraArgs = ["-z"]; # --> Necessary for btrfs
+  
   boot.kernel.sysctl = { "vm.swappiness" = 5;};
 
   boot.tmp.cleanOnBoot = true;
