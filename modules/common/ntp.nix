ba3fb457736cdc590b37ff73d1662a04c9caf0e6
j340m3 <j340m3@github.com>
2025-12-05 09:52:31 +0100

    adding more time servers

--- modules/common/ntp.nix
+++ modules/common/ntp.nix
@@ -9,6 +9,12 @@
       "ptbtime1.ptb.de"
       "time.dfm.dk"
       "time.cifelli.xyz"
+      "ptbtime1.ptb.de"
+      "ptbtime2.ptb.de"
+      "ptbtime3.ptb.de"
+      "ptbtime4.ptb.de"
+      "ntp.zeitgitter.net"
+      "ntp.trifence.ch"
     ];
   };
 }
