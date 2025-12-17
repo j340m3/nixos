fd95c594158fb787b12924febcfcf0fab259c285
j340m3 <j340m3@github.com>
2025-12-17 09:42:38 +0100

    adding scraper

--- modules/prometheus.nix
+++ modules/prometheus.nix
@@ -3,5 +3,15 @@
   services.prometheus = {
     enable = true;
     port = 9001;
+    scrapeConfigs = [
+      {
+        job_name = "woody";
+        static_configs = [{
+          targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.node.port}" ];
+        }];
+      }
+    ];
+
   };
+
 }
