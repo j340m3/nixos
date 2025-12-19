a67a904a8ed9615a86e7cf7a636414b166423870
j340m3 <j340m3@github.com>
2025-12-19 08:45:59 +0100

    ipv6 support better

--- modules/minecraft-bedrock.nix
+++ modules/minecraft-bedrock.nix
@@ -21,14 +21,16 @@
         TZ = "Europe/Berlin";
         VERSION = "LATEST";
         KEEP_INVENTORY = "true";
+        SERVER_PORT_V6=19133
         #OPS = "2535414708374553,2535448442672198"
         #ALLOW_LIST_USERS = "adyxax:2535470760215402,pseudo2:XXXXXXX,pseudo3:YYYYYYY";
       };
       image = "itzg/minecraft-bedrock-server";
-      ports = ["19132:19132/udp" "[::]:19132:19132/udp"];
+      #ports = ["19132:19132/udp" "[::]:19132:19132/udp"];
+      ports = ["19132:19132/udp" "[::]:19133:19133/udp"];
       volumes = [ "/nix/persist/minecraft/klassenserver/:/data" ];
     };
-    emeliebjorn = {
+    /* emeliebjorn = {
       environment = {
         ALLOW_CHEATS = "true";
         EULA = "TRUE";
@@ -44,12 +46,12 @@
       image = "itzg/minecraft-bedrock-server";
       ports = ["19133:19132/udp"];
       volumes = [ "/nix/persist/minecraft/emeliebjorn/:/data" ];
-    };
+    }; */
   };
   networking.firewall.allowedUDPPorts = [
     #53 # DNS
     19132 # Klassenserver
-    19133 # emelie bjorn
+    19133 # Klassenserver v6
   ];
 
   systemd.timers.update-containers = {
