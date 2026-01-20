cc984d3f73b9a1f388885b6e2d3d360a63a896bf
j340m3 <j340m3@github.com>
2026-01-20 13:54:29 +0100

    add firewall ports for reticulum

--- modules/reticulum.nix
+++ modules/reticulum.nix
@@ -8,7 +8,7 @@
     '';
     wantedBy = [ "multi-user.target" ];
   };
-
+  networking.firewall.allowedUDPPorts = [29716 42671];
   # Firewall seems to drop multicast packages
   networking.firewall.extraCommands = ''
     iptables -A nixos-fw -p udp -m pkttype --pkt-type multicast -m udp -j nixos-fw-accept
