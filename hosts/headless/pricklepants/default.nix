a9175f4051a4fc6873b528d86bf119e767a6c79c
j340m3 <j340m3@github.com>
2026-01-27 16:26:49 +0100

    moar path error

--- hosts/headless/pricklepants/default.nix
+++ hosts/headless/pricklepants/default.nix
@@ -212,14 +212,14 @@ in
     path = "/nix/persist/etc/nebula/ca.crt";
   };
   sops.secrets."nebula/self_crt" = {
-    sopsFile = ../../secrets/${config.networking.hostName}/secrets.yaml;
+    sopsFile = ../../../secrets/${config.networking.hostName}/secrets.yaml;
     restartUnits = [ "nebula@mesh.service" ];
     owner = "nebula-mesh";
     group = "nebula-mesh";
     path = "/nix/persist/etc/nebula/self.crt";
   };
   sops.secrets."nebula/self_key" = {
-    sopsFile = ../../secrets/${config.networking.hostName}/secrets.yaml;
+    sopsFile = ../../../secrets/${config.networking.hostName}/secrets.yaml;
     restartUnits = [ "nebula@mesh.service" ];
     owner = "nebula-mesh";
     group = "nebula-mesh";
