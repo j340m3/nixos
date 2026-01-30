a58f780c31c8b3b4784c1b6fc0d1bc5f2eb3fc6d
j340m3 <j340m3@github.com>
2026-01-30 10:11:08 +0100

    rns upgrade

--- modules/reticulum.nix
+++ modules/reticulum.nix
@@ -1,14 +1,33 @@
-{config,lib,pkgs,...}:{
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  rnspython = (
+    pkgs.python3.withPackages (
+      ps: with ps; [
+        rns
+        lxmf
+      ]
+    )
+  );
+in
+{
   systemd.services.reticulum = {
-    path = with pkgs; [
-      rns
-    ];
     script = ''
-      ${pkgs.rns}/bin/rnsd
+      ${rnspython} -m rnsd --service --verbose
     '';
+    #script = ''
+    #  ${pkgs.rns}/bin/rnsd
+    #'';
     wantedBy = [ "multi-user.target" ];
   };
-  networking.firewall.allowedUDPPorts = [29716 42671];
+  networking.firewall.allowedUDPPorts = [
+    29716
+    42671
+  ];
   # Firewall seems to drop multicast packages
   networking.firewall.extraCommands = ''
     iptables -A nixos-fw -p udp -m pkttype --pkt-type multicast -m udp -j nixos-fw-accept
