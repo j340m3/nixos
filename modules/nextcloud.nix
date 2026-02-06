9290da2eb7c79e52f29512e3e07c3b1ea8ecb09a
j340m3 <j340m3@github.com>
2026-02-06 20:35:24 +0100

    rclone not known...

--- modules/nextcloud.nix
+++ modules/nextcloud.nix
@@ -1,4 +1,11 @@
-{ self, config, lib, pkgs, ... }: {
+{
+  self,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
   # Based on https://carjorvaz.com/posts/the-holy-grail-nextcloud-setup-made-easy-by-nixos/
   services = {
     nginx.virtualHosts = {
@@ -15,7 +22,7 @@
         ];
       };
     };
-    # 
+    #
     nextcloud = {
       enable = true;
       hostName = "nextcloud.kauderwels.ch";
@@ -33,14 +40,24 @@
       extraApps = with config.services.nextcloud.package.packages.apps; {
         # List of apps we want to install and are already packaged in
         # https://github.com/NixOS/nixpkgs/blob/master/pkgs/servers/nextcloud/packages/nextcloud-apps.json
-        inherit calendar contacts notes onlyoffice tasks cookbook qownnotesapi;
+        inherit
+          calendar
+          contacts
+          notes
+          onlyoffice
+          tasks
+          cookbook
+          qownnotesapi
+          ;
         # Custom app example.
-        /* socialsharing_telegram = pkgs.fetchNextcloudApp rec {
-          url =
-            "https://github.com/nextcloud-releases/socialsharing/releases/download/v3.0.1/socialsharing_telegram-v3.0.1.tar.gz";
-          license = "agpl3";
-          sha256 = "sha256-8XyOslMmzxmX2QsVzYzIJKNw6rVWJ7uDhU1jaKJ0Q8k=";
-        }; */
+        /*
+          socialsharing_telegram = pkgs.fetchNextcloudApp rec {
+            url =
+              "https://github.com/nextcloud-releases/socialsharing/releases/download/v3.0.1/socialsharing_telegram-v3.0.1.tar.gz";
+            license = "agpl3";
+            sha256 = "sha256-8XyOslMmzxmX2QsVzYzIJKNw6rVWJ7uDhU1jaKJ0Q8k=";
+          };
+        */
       };
       settings = {
         overwriteProtocol = "https";
@@ -65,7 +82,7 @@
     filter = "nextcloud";
     logpath = "/var/log/syslog";
     port = "80,443";
-    banaction = "%(banaction_allports)s"; 
+    banaction = "%(banaction_allports)s";
     maxretry = 3;
     backend = "auto";
     protocol = "tcp";
@@ -79,7 +96,7 @@
     failregex = ^\{%(_groupsre)s,?\s*"remoteAddr":"<HOST>"%(_groupsre)s,?\s*"message":"Login failed:
                 ^\{%(_groupsre)s,?\s*"remoteAddr":"<HOST>"%(_groupsre)s,?\s*"message":"Trusted domain error.
     datepattern = ,?\s*"time"\s*:\s*"%%Y-%%m-%%d[T ]%%H:%%M:%%S(%%z)?"
-    '';
+  '';
 
   sops.secrets."filen/nextcloud.conf" = {
     format = "ini";
@@ -90,6 +107,10 @@
     #path = "/etc/nebula/self.key";
     key = "";
   };
+
+  environment.systemPackages = with pkgs; [
+    rclone
+  ];
 
   fileSystems."/mnt/filen/services/nextcloud" = {
     device = "filen:services/nextcloud";
