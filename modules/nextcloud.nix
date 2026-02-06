8b8854659d51bae8077b421a170cb3b963ff49de
j340m3 <j340m3@github.com>
2026-02-06 19:12:36 +0100

    nextcloud remote connection

--- modules/nextcloud.nix
+++ modules/nextcloud.nix
@@ -81,5 +81,46 @@
     datepattern = ,?\s*"time"\s*:\s*"%%Y-%%m-%%d[T ]%%H:%%M:%%S(%%z)?"
     '';
 
-  
+  sops.secrets."filen/nextcloud.conf" = {
+    format = "ini";
+    sopsFile = ../secrets/services/nextcloud/rclone.ini;
+    #restartUnits = ["nebula@mesh.service"];
+    owner = "nextcloud";
+    group = "nextcloud";
+    #path = "/etc/nebula/self.key";
+    key = "";
+  };
+
+  fileSystems."/mnt/filen/services/nextcloud" = {
+    device = "filen:services/nextcloud";
+    fsType = "rclone";
+    options = [
+      "nodev"
+      #"_netdev"
+      #"nofail"
+      "noauto"
+      "allow_other"
+      "args2env"
+      "x-systemd.automount"
+      "cache_dir=/var/cache/rclone"
+      #"dir-cache-time=24h"
+      "vfs-cache-mode=full"
+      #"vfs-cache-min-free-space=10G"
+      #"vfs-fast-fingerprint"
+      #"vfs-write-back=1m" # write changes after one hour
+      #"vfs-cache-max-age=24h"                    # Retain cached files for up to 24 hours
+      #"vfs-read-chunk-size=32M"                  # Start with 32MB chunks for faster initial reads
+      #"vfs-read-chunk-size-limit=1G"             # Allow chunk size to grow up to 1GB for large files
+      #"vfs-cache-poll-interval=30s"
+      #"tpslimit=16"
+      #"tpslimit-burst=32"
+      "log-level=INFO"
+      "log-file=/var/log/rclone/filen/nextcloud.log"
+      "config=${config.sops.secrets."filen/nextcloud.conf".path}"
+      "x-systemd.requires=network-online.target"
+      "x-systemd.after=network-online.target" # only after network came up
+      "uid=${toString config.users.users.nextcloud.uid}"
+      "gid=${toString config.users.groups.nextcloud.gid}"
+    ];
+  };
 }
