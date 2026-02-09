79de676f88fed49d31bd339932245c2a0109a322
j340m3 <j340m3@github.com>
2026-02-09 10:10:51 +0100

    calm your zramSwap

--- modules/common/zram.nix
+++ modules/common/zram.nix
@@ -1,4 +1,10 @@
-{config,lib,pkgs,...}:{
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
   options = {
     writebackPartition = lib.mkOption {
       type = lib.types.singleLineStr;
@@ -10,17 +16,17 @@
     zramSwap = {
       enable = lib.mkDefault true;
       algorithm = lib.mkDefault "lz4";
-      memoryPercent = 100;
+      memoryPercent = lib.mkDefault 75;
       writebackDevice = lib.mkIf (config.writebackPartition != "") config.writebackPartition;
     };
-    boot.kernel.sysctl = { 
+    boot.kernel.sysctl = {
       "vm.swappiness" = lib.mkDefault 180;
       "vm.watermark_boost_factor" = 0;
       "vm.watermark_scale_factor" = 125;
       "vm.page-cluster" = 0;
-      "vm.dirty_background_ratio"=1;
-      "vm.dirty_ratio"=50;
-      "vm.vfs_cache_pressure"=500;
+      "vm.dirty_background_ratio" = 1;
+      "vm.dirty_ratio" = 50;
+      "vm.vfs_cache_pressure" = 500;
     };
   };
 }
