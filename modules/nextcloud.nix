5f7cd8aa7c404c9f546417ca9451844306ca35db
Jerome Bergmann <j340m3@users.noreply.github.com>
2026-03-05 20:14:36 +0000

    Update nextcloud.nix

--- modules/nextcloud.nix
+++ modules/nextcloud.nix
@@ -33,7 +33,7 @@
       home = "/var/lib/nextcloud";
       hostName = "nextcloud.kauderwels.ch";
       # Need to manually increment with every major upgrade.
-      package = pkgs.nextcloud33;
+      package = pkgs.nextcloud32;
       # Let NixOS install and configure the database automatically.
       database.createLocally = true;
       # Let NixOS install and configure Redis caching automatically.
