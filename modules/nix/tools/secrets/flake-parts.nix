b904601cbbb7ae0f8f5460e73b8376739ccc72d5
j340m3 <j340m3@users.noreply.github.com>
2026-03-12 16:11:51 +0100

    debugging sops-nix

--- modules/nix/tools/secrets/flake-parts.nix
+++ modules/nix/tools/secrets/flake-parts.nix
@@ -6,6 +6,7 @@
   flake-file.inputs = {
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
+    sops-nix.inputs.home-manager.follows = "home-manager";
   };
 
   imports = [ inputs.sops-nix.nixosModules.sops ];
