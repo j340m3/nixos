cade5f6c7aec6ff1f62c5c8b475d45635ac13c68
j340m3 <j340m3@users.noreply.github.com>
2026-03-12 14:13:14 +0100

    added some other stuff

---
+++ modules/minimal.nix
@@ -0,0 +1,21 @@
+{
+  inputs,
+  ...
+}:
+{
+  # default settings needed for all nixosConfigurations
+
+  flake.modules.nixos.system-minimal =
+    { pkgs, ... }:
+    {
+      nixpkgs.overlays = [
+        (final: _prev: {
+          unstable = import inputs.nixpkgs {
+            inherit (final) config;
+            system = pkgs.stdenv.hostPlatform.system;
+          };
+        })
+      ];
+      system.stateVersion = "25.05";
+    };
+}
