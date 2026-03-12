77ba37725554ba198a6e1a383c64386ab7b75879
j340m3 <j340m3@users.noreply.github.com>
2026-03-12 15:48:51 +0100

    secrets handling

--- flake.nix
+++ flake.nix
@@ -6,6 +6,10 @@
   inputs = {
     flake-file.url = "github:vic/flake-file";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     import-tree.url = "github:vic/import-tree";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
