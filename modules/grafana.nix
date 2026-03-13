cafbb39fbd726478cc1e26d25ba37496b03adfd0
j340m3 <j340m3@users.noreply.github.com>
2026-03-13 16:18:41 +0100

    security key is not default anymore

--- modules/grafana.nix
+++ modules/grafana.nix
@@ -1,21 +1,27 @@
-{ config, pkgs, lib, constants, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  constants,
+  ...
+}:
 {
   services.grafana = {
     enable = true;
     domain = "grafana.kauderwels.ch";
     port = 2342;
     addr = "0.0.0.0";
+    settings.security.secret_key = "SW2YcwTIb9zpOOhoPsMm";
   };
   services.nginx.virtualHosts.${config.services.grafana.domain} = {
     locations."/" = {
-        proxyPass = "http://127.0.0.1:${toString config.services.grafana.port}";
-        proxyWebsockets = true;
+      proxyPass = "http://127.0.0.1:${toString config.services.grafana.port}";
+      proxyWebsockets = true;
     };
   };
-  networking.firewall.interfaces."nebula.mesh".allowedTCPPorts = [ config.services.grafana.port];
-  services.nebula.networks.mesh.firewall.inbound = lib.mkIf 
-              (config.services.grafana.enable && 
-              config.services.nebula.networks.mesh.enable) 
+  networking.firewall.interfaces."nebula.mesh".allowedTCPPorts = [ config.services.grafana.port ];
+  services.nebula.networks.mesh.firewall.inbound =
+    lib.mkIf (config.services.grafana.enable && config.services.nebula.networks.mesh.enable)
       [
         {
           cidr = constants.nebula.cidr;
