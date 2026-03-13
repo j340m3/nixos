f6d3c61ef296e00f59df7fafac947d1a09bf8bb3
j340m3 <j340m3@users.noreply.github.com>
2026-03-13 15:32:29 +0100

    formatting

--- modules/users/jeromeb/style.nix
+++ modules/users/jeromeb/style.nix
@@ -1,41 +1,44 @@
-{inputs,...}:{
-  flake.modules.homeManager.jeromeb-style = {pkgs,...}:{
-    stylix.autoEnable = true;
-  stylix.enable = true;
-  # FIXME
-  #stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
-  stylix.base16Scheme = "${inputs.tinted-schemes}/base16/catppuccin-frappe.yaml";
-  #stylix.polarity = "light";
-  stylix.fonts = {
-    monospace = {
-      package = pkgs.nerd-fonts.victor-mono;
-      name = "VictorMono Nerd Font Mono";
-    };
-    # sansSerif = {
-    #   package = inputs.apple-fonts.packages.${pkgs.system}.sf-pro-nerd;
-    #   name = "SFProDisplay Nerd Font";
-    # };
-    # serif = {
-    #   package = inputs.apple-fonts.packages.${pkgs.system}.sf-pro-nerd;
-    #   name = "SFProDisplay Nerd Font";
-    # };
-    emoji = {
-      package = pkgs.noto-fonts-color-emoji;
-      name = "Noto Color Emoji"; 
-    };
-  };
-  stylix.cursor = {
-    name = "macOS";
-    package = pkgs.apple-cursor;
-    size = 36;
-  };
-  stylix.icons = {
-    enable = true;
-    package = pkgs.whitesur-icon-theme;
-    dark = "WhiteSur-dark";
-    light = "WhiteSur-light";
-  };
+{ inputs, ... }:
+{
+  flake.modules.homeManager.jeromeb-style =
+    { pkgs, ... }:
+    {
+      stylix.autoEnable = true;
+      stylix.enable = true;
+      # FIXME
+      #stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+      stylix.base16Scheme = "${inputs.tinted-schemes}/base16/catppuccin-frappe.yaml";
+      #stylix.polarity = "light";
+      stylix.fonts = {
+        monospace = {
+          package = pkgs.nerd-fonts.victor-mono;
+          name = "VictorMono Nerd Font Mono";
+        };
+        # sansSerif = {
+        #   package = inputs.apple-fonts.packages.${pkgs.system}.sf-pro-nerd;
+        #   name = "SFProDisplay Nerd Font";
+        # };
+        # serif = {
+        #   package = inputs.apple-fonts.packages.${pkgs.system}.sf-pro-nerd;
+        #   name = "SFProDisplay Nerd Font";
+        # };
+        emoji = {
+          package = pkgs.noto-fonts-color-emoji;
+          name = "Noto Color Emoji";
+        };
+      };
+      stylix.cursor = {
+        name = "macOS";
+        package = pkgs.apple-cursor;
+        size = 36;
+      };
+      stylix.icons = {
+        enable = true;
+        package = pkgs.whitesur-icon-theme;
+        dark = "WhiteSur-dark";
+        light = "WhiteSur-light";
+      };
 
-  stylix.targets.firefox.profileNames = [ "personal" ];
-  };
+      stylix.targets.firefox.profileNames = [ "personal" ];
+    };
 }
