6be0e5e507d6f4a3b631cdc8193f43af7a7ace0a
Jerome Bergmann <j340m3@users.noreply.github.com>
2026-03-25 15:43:37 +0000

    Update nextcloud.nix

--- modules/nextcloud.nix
+++ modules/nextcloud.nix
@@ -99,6 +99,17 @@
       startAt = "*-*-* 01:15:00";
     };
   };
+  security.acme = {
+      acceptTerms = true;
+      defaults = {
+        email = "jerome.bergmann@posteo.de";
+        #dnsProvider = "cloudflare";
+        # location of your CLOUDFLARE_DNS_API_TOKEN=[value]
+        # https://www.freedesktop.org/software/systemd/man/latest/systemd.exec.html#EnvironmentFile=
+        #environmentFile = "/REPLACE/WITH/YOUR/PATH";
+      };
+    };
+
   services.fail2ban.jails."nextcloud".settings = {
     enabled = true;
     filter = "nextcloud";
