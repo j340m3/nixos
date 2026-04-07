6394e37b3f6225616b0bb20c8948a8ad182c7b86
j340m3 <j340m3@users.noreply.github.com>
2026-04-07 12:32:19 +0200

    added stuff

---
+++ modules/services/minetest.nix
@@ -0,0 +1,60 @@
+{
+  flake.modules.nixos.minetest =
+    {
+      config,
+      pkgs,
+      lib,
+      ...
+    }:
+
+    {
+      environment.systemPackages = with pkgs; [
+        borgbackup
+      ];
+
+      services.minetest-server = {
+        enable = true;
+        port = 30000;
+        gameId = "voxelibre";
+        config = {
+          enable_rollback = true;
+          sprint_speed = 10;
+          hudbars_autohide_stamina = false;
+          address = "minetest.kauderwels.ch";
+          remote_port = 30000;
+          server_announce = false;
+          server_name = "Bergmänners";
+          server_description = "Se server wo se Bergmänner hosten";
+          enable_damage = true;
+          creative_mode = false;
+          name = "kauderwelsch";
+          enable_ipv6 = true;
+          ipv6_server = true;
+          enable_fire = true;
+        };
+      };
+      networking.firewall.allowedTCPPorts = [ 30000 ];
+      networking.firewall.allowedUDPPorts = [ 30000 ];
+
+      sops.secrets."borg/luanti" = {
+        sopsFile = ../secrets/hosts/${config.networking.hostName}/secrets.yaml;
+      };
+
+      services.borgbackup.jobs.minetest = {
+        paths = "/var/lib/minetest/.minetest";
+        encryption.mode = "none";
+        environment.BORG_RSH = "ssh -i ${config.sops.secrets."borg/luanti".path}";
+        repo = "borg@10.0.0.3:.";
+        compression = "auto,lzma";
+        startAt = "daily";
+        preHook = "systemctl stop minetest-server";
+        postHook = "systemctl start minetest-server";
+      };
+
+      # Send an email whenever auto upgrade fails
+      systemd.services."borgbackup-job-minetest".onFailure =
+        lib.mkIf config.systemd.services."notify-telegram@".enable
+          [ "notify-telegram@%i.service" ];
+
+    };
+}
---
+++ modules/services/vaultwarden.nix
@@ -0,0 +1,85 @@
+{
+  flake.modules.nixos.vaultwarden =
+    {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
+
+    with lib;
+    let
+      domainName = "vaultwarden.kauderwels.ch";
+    in
+    {
+      services.vaultwarden = {
+        enable = true;
+        config = {
+          ROCKET_ADDRESS = "127.0.0.1";
+          ROCKET_PORT = 8222;
+          DOMAIN = "https://${domainName}";
+        };
+        backupDir = "/var/backup/vaultwarden";
+      };
+      services.nginx = {
+        enable = true;
+
+        # Use recommended settings
+        recommendedGzipSettings = true;
+
+        virtualHosts."vaultwarden.kauderwels.ch" = {
+          forceSSL = true;
+          #enableACME = true;
+          #useACMEHost = domainName;
+          #locations."/.well-known/".root = "/var/lib/acme/acme-challenge/";
+          locations."/" = {
+            proxyPass = "http://127.0.0.1:${toString config.services.vaultwarden.config.ROCKET_PORT}";
+          };
+          # sslCertificate = "/etc/ssl/certs/kauderwels.ch_ssl_certificate.cer";
+          sslCertificate = "/etc/ssl/certs/kauderwels.ch_ssl_certificate_chain.cer";
+          sslCertificateKey = "/etc/ssl/certs/_.kauderwels.ch_private_key.key";
+        };
+      };
+      networking.firewall.allowedTCPPorts = [
+        443
+        80
+      ];
+
+      #security.acme = {
+      #    acceptTerms = true;
+      #    defaults = {
+      #      email = "jerome.bergmann@posteo.de";
+      #      webroot = "/var/lib/acme/acme-challenge/";
+      #      #dnsProvider = "cloudflare";
+      #      # location of your CLOUDFLARE_DNS_API_TOKEN=[value]
+      #      # https://www.freedesktop.org/software/systemd/man/latest/systemd.exec.html#EnvironmentFile=
+      #      #environmentFile = "/REPLACE/WITH/YOUR/PATH";
+      #    };
+      #    certs.${domainName}.group = config.services.nginx.group;
+      #  };
+
+      # for acme plain http challenge
+      # networking.firewall.allowedTCPPorts = [ 80 ];
+
+      services.fail2ban.jails."vaultwarden".settings = {
+        enabled = true;
+        filter = "vaultwarden";
+        logpath = "/var/log/syslog";
+        port = "80,443,8081";
+        banaction = "%(banaction_allports)s";
+        maxretry = 3;
+        bantime = 14400;
+        findtime = 14400;
+      };
+
+      environment.etc."fail2ban/filter.d/vaultwarden.local".text = ''
+        [INCLUDES]
+        before = common.conf
+
+        [Definition]
+        failregex = ^.*?Username or password is incorrect\. Try again\. IP: <ADDR>\. Username:.*$
+        ignoreregex =
+      '';
+    };
+
+}
