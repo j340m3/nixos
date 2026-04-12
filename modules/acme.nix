ee8d6decfc998540adeb6fa3d94273c8f6b31c05
j340m3 <j340m3@github.com>
2026-04-12 21:09:41 +0200

    acme.nix was missing

---
+++ modules/acme.nix
@@ -0,0 +1,20 @@
+{ ... }:
+let
+  domainName = "kauderwels.ch";
+in
+{
+  security.acme = {
+    acceptTerms = true;
+    defaults = {
+      email = "jerome.bergmann@posteo.de";
+      dnsProvider = "ionos";
+      # location of your CLOUDFLARE_DNS_API_TOKEN=[value]
+      # https://www.freedesktop.org/software/systemd/man/latest/systemd.exec.html#EnvironmentFile=
+      environmentFile = "/var/lib/acme/api_key";
+    };
+    certs.${domainName}.extraDomainNames = [
+      "vaultwarden.kauderwels.ch"
+      "nextcloud.kauderwels.ch"
+    ];
+  };
+}
