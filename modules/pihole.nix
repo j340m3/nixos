bd61173a6c4ce1c67f0219e39d0f865835c606f4
j340m3 <j340m3@github.com>
2026-05-08 21:22:52 +0200

    switching to dnsdist since adguardhome coredumps...

--- modules/pihole.nix
+++ modules/pihole.nix
@@ -1,67 +1,80 @@
 { ... }:
 {
-  services.adguardhome = {
-    enable = true;
-    settings = {
-      dns = {
-        bind_hosts = [
-          "0.0.0.0"
-        ];
-        anonymize_client_ip = true;
-        #filtering_enabled = true;
-        upstream_dns = [
-          "tls://dns3.digitalcourage.de"
-          "tls://dot.ffmuc.net"
-          "tls://dns.digitale-gesellschaft.ch"
-          "https://doh.ffmuc.net/dns-query"
-          "https://dns.digitale-gesellschaft.ch/dns-query"
-        ];
-        bootstrap_dns = [
-          "5.1.66.255" # FFMUC
-          "1.1.1.1"
-          "1.0.0.1"
-          "9.9.9.9"
-        ];
-        fallback_dns = [
-          "tls://anycast.uncensoreddns.org"
-          "tls://unfiltered.adguard-dns.com"
-          "https://unfiltered.adguard-dns.com/dns-query"
-          "tls://dns.njal.la"
-        ];
-        upstream_mode = "parallel";
-        use_http3_upstreams = true;
-        use_dns64 = true;
-        cache_enabled = true;
-        cache_optimistic = true;
-        cache_size = 1048576;
-      };
+  /*
+    services.adguardhome = {
+      enable = true;
+      settings = {
+        dns = {
+          bind_hosts = [
+            "0.0.0.0"
+          ];
+          anonymize_client_ip = true;
+          #filtering_enabled = true;
+          upstream_dns = [
+            "tls://dns3.digitalcourage.de"
+            "tls://dot.ffmuc.net"
+            "tls://dns.digitale-gesellschaft.ch"
+            "https://doh.ffmuc.net/dns-query"
+            "https://dns.digitale-gesellschaft.ch/dns-query"
+          ];
+          bootstrap_dns = [
+            "5.1.66.255" # FFMUC
+            "1.1.1.1"
+            "1.0.0.1"
+            "9.9.9.9"
+          ];
+          fallback_dns = [
+            "tls://anycast.uncensoreddns.org"
+            "tls://unfiltered.adguard-dns.com"
+            "https://unfiltered.adguard-dns.com/dns-query"
+            "tls://dns.njal.la"
+          ];
+          upstream_mode = "parallel";
+          use_http3_upstreams = true;
+          use_dns64 = true;
+          cache_enabled = true;
+          cache_optimistic = true;
+          cache_size = 1048576;
+        };
 
-      tls = {
-        enabled = false;
-        server_name = "frei.kauderwels.ch";
-      };
-      querylog = {
-        file_enabled = false;
-        interval = "48h";
-      };
-      dhcp.enabled = false;
-      filters = [
-        {
-          enabled = true;
-          url = "https://raw.githubusercontent.com/j340m3/anti-parental-lists/refs/heads/main/family-link.txt";
-          id = 1;
-        }
-      ];
-      log = {
-        verbose = true;
-        compress = true;
-        file = "/tmp/ahglog.txt";
+        tls = {
+          enabled = false;
+          server_name = "frei.kauderwels.ch";
+        };
+        querylog = {
+          file_enabled = false;
+          interval = "48h";
+        };
+        dhcp.enabled = false;
+        filters = [
+          {
+            enabled = true;
+            url = "https://raw.githubusercontent.com/j340m3/anti-parental-lists/refs/heads/main/family-link.txt";
+            id = 1;
+          }
+        ];
+        log = {
+          verbose = true;
+          compress = true;
+          file = "/tmp/ahglog.txt";
+        };
       };
     };
-  };
+  */
 
   networking.firewall = {
     allowedTCPPorts = [ 53 ];
     allowedUDPPorts = [ 53 ];
+  };
+
+  services.dnsdist = {
+    enable = true;
+    extraConfig = ''
+      addLocal("0.0.0.0:53")
+      addLocal("[::]:53")
+      newServer("1.1.1.1")
+      pc = newPacketCache(100000)
+      getPool(""):setCache(pc)
+    '';
   };
 }
